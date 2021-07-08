54d3392220fa53016d086ad5cf2f88f35fa43c29
Roselle Tabuena <roselleroxtabuena@gmail.com>
2021-07-08 12:44:15 +0800

    changed render.rb to renderer.rb

--- lib/devcampr_view_tool.rb
+++ lib/devcampr_view_tool.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "devcampr_view_tool/version"
-
+require_relative "devcampr_view_tool/renderer"
 module DevcamprViewTool
   class Error < StandardError; end
   # Your code goes here...
